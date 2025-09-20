bfe4deba1d31f20595bea89e8160f19bb1e7d1d6
m_kolobanova <m.kolobanova@pay-lab.ru>
2025-09-20 14:02:03 +0300

    Migrate from callback to suspend fun and coroutines

--- app/src/main/java/otus/homework/coroutines/CatsPresenter.kt
+++ app/src/main/java/otus/homework/coroutines/CatsPresenter.kt
@@ -1,28 +1,35 @@
 package otus.homework.coroutines
 
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+import otus.homework.coroutines.CrashMonitor.trackWarning
+import java.net.SocketTimeoutException
+
 
 class CatsPresenter(
-    private val catsService: CatsService
+    private val catsService: CatsService,
+    private val onShowToast: (String?) -> Unit
 ) {
 
     private var _catsView: ICatsView? = null
-
+    private val presenterScope = CoroutineScope(Dispatchers.Main + CoroutineName("CatsCoroutine"))
     fun onInitComplete() {
-        catsService.getCatFact().enqueue(object : Callback<Fact> {
+        presenterScope.launch {
+            try {
+                val fact = catsService.getCatFact()
+                _catsView?.populate(fact)
+            } catch (e: SocketTimeoutException) {
+                println("Caught $e")
+                onShowToast("Не удалось получить ответ от сервера")
 
-            override fun onResponse(call: Call<Fact>, response: Response<Fact>) {
-                if (response.isSuccessful && response.body() != null) {
-                    _catsView?.populate(response.body()!!)
-                }
+            } catch (e: Exception) {
+                trackWarning(e.message)
+                onShowToast(e.message)
             }
-
-            override fun onFailure(call: Call<Fact>, t: Throwable) {
-                CrashMonitor.trackWarning()
-            }
-        })
+        }
     }
 
     fun attachView(catsView: ICatsView) {
@@ -31,5 +38,9 @@ class CatsPresenter(
 
     fun detachView() {
         _catsView = null
+    }
+
+    fun onStop() {
+        presenterScope.cancel()
     }
 }
--- app/src/main/java/otus/homework/coroutines/MainActivity.kt
+++ app/src/main/java/otus/homework/coroutines/MainActivity.kt
@@ -2,6 +2,10 @@ package otus.homework.coroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,8 +18,7 @@ class MainActivity : AppCompatActivity() {
 
         val view = layoutInflater.inflate(R.layout.activity_main, null) as CatsView
         setContentView(view)
-
-        catsPresenter = CatsPresenter(diContainer.service)
+        catsPresenter = CatsPresenter(diContainer.service, ::onShowToast)
         view.presenter = catsPresenter
         catsPresenter.attachView(view)
         catsPresenter.onInitComplete()
@@ -24,7 +27,12 @@ class MainActivity : AppCompatActivity() {
     override fun onStop() {
         if (isFinishing) {
             catsPresenter.detachView()
+            catsPresenter.onStop()
         }
         super.onStop()
+    }
+
+    private fun onShowToast(message: String?) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
